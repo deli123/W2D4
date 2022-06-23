a2ffd54471444d6b05a6aafb6844796ddd138a55
Derek <liderek63@gmail.com>
2022-06-23 11:49:28 -0700

    completed problems.rb

--- recap_exercise_2/lib/problems.rb
+++ recap_exercise_2/lib/problems.rb
@@ -1,21 +1,41 @@
 # Write a method, least_common_multiple, that takes in two numbers and returns the smallest number that is a mutiple
 # of both of the given numbers
 def least_common_multiple(num_1, num_2)
-
+    mult_1 = num_1
+    mult_2 = num_2
+    while mult_1 % mult_2 != 0
+        if mult_1 < mult_2
+            mult_1 += num_1
+        else
+            mult_2 += num_2
+        end
+    end
+    return mult_1
 end
 
 
 # Write a method, most_frequent_bigram, that takes in a string and returns the two adjacent letters that appear the
 # most in the string.
 def most_frequent_bigram(str)
-
+    count = Hash.new(0)
+    i = 0
+    while i < str.length - 1
+        count[str[i..i+1]] += 1
+        i += 1
+    end
+    # return the key that corresponds to the max value
+    return count.max_by { |k, v| v}[0]
 end
 
 
 class Hash
     # Write a method, Hash#inverse, that returns a new hash where the key-value pairs are swapped
     def inverse
-
+        new_hash = {}
+        self.each do |k, v|
+            new_hash[v] = k
+        end
+        return new_hash
     end
 end
 
@@ -23,7 +43,17 @@ end
 class Array
     # Write a method, Array#pair_sum_count, that takes in a target number returns the number of pairs of elements that sum to the given target
     def pair_sum_count(num)
-
+        count = 0
+        self.each_with_index do |ele_1, i|
+            self.each_with_index do |ele_2, j|
+                if i < j
+                    if ele_1 + ele_2 == num
+                        count += 1
+                    end
+                end
+            end
+        end
+        return count
     end
 
     # Write a method, Array#bubble_sort, that takes in an optional proc argument.
@@ -40,6 +70,22 @@ class Array
     #
     # This should remind you of the spaceship operator! Convenient :)
     def bubble_sort(&prc)
+        prc ||= Proc.new { |el_1, el_2| el_1 <=> el_2 }
 
+        sorted = false
+
+        while !sorted
+            sorted = true
+
+            i = 0
+            while i < self.length - 1
+                if prc.call(self[i], self[i+1]) == 1
+                    self[i], self[i+1] = self[i+1], self[i]
+                    sorted = false
+                end
+                i += 1
+            end
+        end
+        return self
     end
 end
